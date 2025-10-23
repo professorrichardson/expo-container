36f86a9392b18de3b567b8a449b41df85da62dd6
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2025-10-23 14:10:54 +0000

    aula02

---
+++ componentes/Titulo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Titulo(){
+    return(
+        <Text style={{ fontSize:24, fontWeight:'bold', marginBottom:10}}>
+            Bem-Vindo ao React Native!
+        </Text>
+    )
+}
